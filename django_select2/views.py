e27fe2d55505189a4174487babaa2eebf49e05ac
Viktor <viktor.vraniak@gmail.com>
2021-09-03 15:34:37 +0200

    Dont hardcode __in filter for multivlaue fields

--- django_select2/views.py
+++ django_select2/views.py
@@ -51,20 +51,11 @@ class AutoResponseView(BaseListView):
         kwargs = {
             model_field_name: self.request.GET.get(form_field_name)
             for form_field_name, model_field_name in self.widget.dependent_fields.items()
+            if form_field_name in self.request.GET
+            and self.request.GET.get(form_field_name, "") != ""
         }
-        kwargs.update(
-            {
-                f"{model_field_name}__in": self.request.GET.getlist(
-                    f"{form_field_name}[]", []
-                )
-                for form_field_name, model_field_name in self.widget.dependent_fields.items()
-            }
-        )
         return self.widget.filter_queryset(
-            self.request,
-            self.term,
-            self.queryset,
-            **{k: v for k, v in kwargs.items() if v},
+            self.request, self.term, self.queryset, **kwargs
         )
 
     def get_paginate_by(self, queryset):
